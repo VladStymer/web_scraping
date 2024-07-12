9d461ea7e7ff5f18b50b1d3cb96cf15b5e7d989e
VladStymer <vnicoud@yahoo.fr>
2024-07-12 14:28:41 +0200

    url

--- new_scrap/main.py
+++ new_scrap/main.py
@@ -6,6 +6,6 @@ def main():
     # print that the script is starting
     print("Starting the script...")
     scrap.main()
-
+# https://www.autoscout24.ch/fr/s?hadNoAccidentOnly=true&fuelTypes%5B0%5D=petrol&fuelTypes%5B1%5D=hev-petrol&fuelTypes%5B2%5D=mhev-petrol&fuelTypes%5B3%5D=phev-petrol&fuelTypes%5B4%5D=electric&horsePowerFrom=100&cubicCapacityFrom=1000&pagination%5Bpage%5D=1
 if __name__ == "__main__":
     main()
