74438800c914e973bd07f59a90f21ed4cb9f616e
Stymer <87383665+VladStymer@users.noreply.github.com>
2024-07-12 16:34:22 +0200

    working

--- new_scrap/main.py
+++ new_scrap/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 import scrap
+import time
 
 def main():
     # print that the script is starting
     print("Starting the script...")
-    scrap.main()
-# https://www.autoscout24.ch/fr/s?hadNoAccidentOnly=true&fuelTypes%5B0%5D=petrol&fuelTypes%5B1%5D=hev-petrol&fuelTypes%5B2%5D=mhev-petrol&fuelTypes%5B3%5D=phev-petrol&fuelTypes%5B4%5D=electric&horsePowerFrom=100&cubicCapacityFrom=1000&pagination%5Bpage%5D=1
+    time.sleep(10)
+    # Call the correct function with a base URL parameter
+    #base_url = "https://www.autoscout24.ch/fr/s?hadNoAccidentOnly=true&fuelTypes%5B0%5D=petrol&fuelTypes%5B1%5D=hev-petrol&fuelTypes%5B2%5D=mhev-petrol&fuelTypes%5B3%5D=phev-petrol&fuelTypes%5B4%5D=electric&horsePowerFrom=100&cubicCapacityFrom=1000"
+    base_url = "https://www.autoscout24.ch/fr/s?hadNoAccidentOnly=true&fuelTypes%5B0%5D=petrol&fuelTypes%5B1%5D=hev-petrol&fuelTypes%5B2%5D=mhev-petrol&fuelTypes%5B3%5D=phev-petrol&fuelTypes%5B4%5D=electric&horsePowerFrom=100&cubicCapacityFrom=1000&mileageTo=4000&priceFrom=275000&priceTo=300000"
+    urls = scrap.fetch_vehicle_urls(base_url)
+    # print the number of URLs found
+    print(f"Found {len(urls)} URLs")
+
+
 if __name__ == "__main__":
     main()
--- new_scrap/scrap.py
+++ new_scrap/scrap.py
@@ -1,41 +1,58 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
+import time
 
 def fetch_vehicle_urls(base_url):
-    # Configuration pour utiliser Remote WebDriver
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+	options = webdriver.ChromeOptions()
+	options.headless = True
+	options.add_argument("--no-sandbox")
+	options.add_argument("--disable-dev-shm-usage")
+	options.set_capability("browserName", "chrome")
+	options.set_capability("browserVersion", "126.0")
+	options.set_capability("platformName", "Linux")
 
-    driver = webdriver.Remote(
-        command_executor='http://chrome:4444/wd/hub',  # Notez l'adresse du Selenium server dans Docker
-        desired_capabilities=DesiredCapabilities.CHROME,
-        options=options
-    )
+	driver = webdriver.Remote(
+		command_executor='http://chrome:4444/wd/hub',
+		options=options
+	)
 
-    all_urls = []
-    max_pages = 6000  # Nombre maximum de pages à scraper
-    for page in range(1, max_pages + 1):
-        print("In the while loop")
-        first_url = f"{base_url}"
-        current_url = f"{base_url}&pagination%5Bpage%5D={page}"
-        if page == 1:
-            current_url = first_url
-        driver.get(current_url)
-        
-        # Utiliser Selenium pour extraire les URLs
-        elements = driver.find_elements_by_xpath('//*[@id="__next"]/div/div[2]/div[1]/main/ul/li/article/a')
-        if not elements:
-            print(f"No URLs found on page {page}")
-            break
-        
-        for element in elements:
-            url = element.get_attribute('href')
-            print(f"Found URL: {url}")
-            all_urls.append(url)
+	all_urls = []
+	max_pages = 600
+	for page in range(0, max_pages + 1):
+		urlcount = 0
+		print("In the while loop")
+		first_url = f"{base_url}"
+		current_url = f"{base_url}&pagination%5Bpage%5D={page}"
+		if page == 0:
+			current_url = first_url
+		driver.get(current_url)
 
-        time.sleep(1)  # Delay pour simuler le comportement humain
+		DEBUG = False
+		if DEBUG:
+			print(f"Starting DEBUG")
+			time.sleep(5)  # Attendez que la page charge complètement
+			driver.save_screenshot(f"/app/data/page_{page}.png")
+			html_content = driver.page_source
+			with open(f"/app/data/page_{page}.html", 'w', encoding='utf-8') as f:
+				f.write(html_content)  # Sauvegarde le HTML de la page dans un fichier
+		
+		elements = driver.find_elements(By.XPATH, '//*[@id="__next"]/div/div[2]/div[1]/main/ul/li/article/a')
+		if not elements:
+			print(f"No URLs found on page {page}")
+			break
+		
+		for element in elements:
+			url = element.get_attribute('href')
+			print(f"Found URL: {url}")
+			all_urls.append(url)
+			# print le nombre d'URLs trouvées
+			urlcount += 1
+		if urlcount < 20:
+			print(f"Last page is the {page}")
+			break
+		print(f"{urlcount} URLs found on page {page}")
+		print()
+		time.sleep(1)  # Delay pour simuler le comportement humain
 
-    driver.quit()
-    return all_urls
+	driver.quit()
+	return all_urls
